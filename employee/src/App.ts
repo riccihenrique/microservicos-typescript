5afa785950dac76fc7b428492169036781655f26
Henrique Ricci <ricci.henrique@outlook.com>
2022-06-11 13:03:00 -0300

    init employee project

---
+++ employee/src/App.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from 'express';
+import ErrorBase from './errors/ErrorBase';
+
+class App {
+    private app;
+    constructor() {
+      this.app = express();
+
+      this.middlewares();
+      this.routes();
+      this.errorMiddleware();
+    }
+
+    listen(port: string) {
+      this.app.listen(port, () => console.log(`Running on ${port} port.`));
+    }
+
+    private middlewares() {
+        this.app.use(express.json());
+    }
+
+    private errorMiddleware() {
+      this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        if(err instanceof ErrorBase) {
+          return res.status(err.stausCode).json({ message: err.message });
+        }
+        console.log(err);
+        res.status(500).json({ message: 'Internal error' });
+      });
+    }
+
+    private routes() {
+      
+    }
+}
+
+export default App;
